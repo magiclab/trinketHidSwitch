1b25c4b2f422575f861604702a6aae81847cbbbf
enrico viola <naus3a@gmail.com>
2017-03-30 16:20:24 +0200

    ping

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ trinketHidToggle
 A [Trinket](https://learn.adafruit.com/introducing-trinket/introduction) firmware that sets the tiny board as a [HID device](https://en.wikipedia.org/wiki/Human_interface_device), so it's possible to send and receive messages via USB.
 
 The sketch implements a very simple protocol:
-- send 0 or 1 (as an unsigned char) to turn pin 1 off/on
-- receive 0 or 1 (as an unsigned char) whenever pin 1 changes state
+- send 0 or 1 (as an unsigned char) to turn pin 2 off/on
+- receive 0 or 1 (as an unsigned char) whenever pin 2 changes state
+
+The LED connected to pin 1 turns on/off to show pin 2 status; it also blinks when the Trinket receives a ping message (any byte different from 0 or 1).
 
 Dependencies
 ------------
--- trinketHidSwitch.ino
+++ trinketHidSwitch.ino
@@ -2,28 +2,57 @@
 
 HIDSerial serial;
 bool bState = false;
-int pinPower = 1;
+bool bPingLight = false;
+int nFrames = 0;
+const int maxFrames = 20000;
+int pinLed = 1;
+int pinPower = 2;
 unsigned char buffer[1];
 
 void setup() {
   bState = false;
+  bPingLight = false;
+  nFrames = 0;
   pinMode(pinPower, OUTPUT);
-  digitalWrite(pinPower, LOW);
+  digitalWrite(pinPower, HIGH);
   serial.begin();
 }
 
 void loop() {
+  if(bPingLight){
+    nFrames++;
+    if(nFrames>maxFrames){
+      bPingLight=false;
+      nFrames = 0;
+      if(bState==false){
+        digitalWrite(pinLed, LOW);
+      }
+      serial.write('r');
+      serial.poll();
+      return;
+    }
+  }
+  
   if(serial.available()) {
     int size = serial.read(buffer);
     if (size>0) {
       if(buffer[0]=='0'){
         bState = false;
-        digitalWrite(pinPower, LOW);
+        digitalWrite(pinLed, LOW);
+        digitalWrite(pinPower, HIGH);
         serial.write('0');
       }else if(buffer[0]=='1'){
         bState = true;
-        digitalWrite(pinPower, HIGH);
+        digitalWrite(pinLed, HIGH);
+        digitalWrite(pinPower, LOW);
         serial.write('1');
+      }else{
+        if(bPingLight==false){
+          bPingLight = true;
+          nFrames = 0;
+          digitalWrite(pinLed,HIGH);
+          serial.write('p');
+        }
       }
     }
   }
